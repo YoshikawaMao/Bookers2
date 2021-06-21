4aafed84df7d6a97132126fb5b3f606fa6821f3b
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-21 10:04:10 +0000

    [add]userのeditページとupdate機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eaf607fc7e54e31c54466bdc299dc732eb3a82916b91c16cdcc078395f72dc0fc507b74dc3ad6cc75c153d125df69c139018a6534fe884ce2346a30be70d5dcb'
