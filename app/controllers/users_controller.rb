89172b8b5f85f8cda763ec80ad543a9d57a28267
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-21 06:44:30 +0000

    [add]userのindex,showページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
    def index
       @user = current_user
+      @book = Book.new
+      @users = User.all
    end
 
    def show
+      @user = User.find(params[:id])
+      @book_new = Book.new
+      @books = @user.books
    end
 
    def edit
