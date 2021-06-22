0c71b2ae02ebf7ee66d30a6c3a85bb7786f7d4fd
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-22 05:04:07 +0000

    [add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,13 +14,18 @@ class BooksController < ApplicationController
 
      def create
       # １. データを新規登録するためのインスタンス作成
+      @user = current_user
+      @books = Book.all
       @book = Book.new(book_params)
       @book.user_id = current_user.id
       # ２. データをデータベースに保存するためのsaveメソッド実行
-      @book.save
-      flash[:notice] = "You have create book succesfully."
-      # ３.showページに飛ぶ
-      redirect_to book_path(@book)
+      if @book.save
+         flash[:notice] = "You have create book succesfully."
+         # ３.showページに飛ぶ
+         redirect_to book_path(@book)
+      else
+         render :index
+      end
      end
 
 
@@ -30,9 +35,12 @@ class BooksController < ApplicationController
 
      def update
          @book = Book.find(params[:id])
-         @book.update(book_params)
-         flash[:notice] = "You have updated book succesfully."
-         redirect_to book_path(@book)
+         if @book.update(book_params)
+             flash[:notice] = "You have updated book succesfully."
+             redirect_to book_path(@book)
+         else
+             render :edit
+         end
      end
 
      def destroy
