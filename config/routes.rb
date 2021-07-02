74660cbd76656f99bab7f13cb6202c01c1ce8f9a
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-07-02 12:32:18 +0000

    いいね・コメント機能作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   get "home/about" => "homes#about"
   get "top" => "homes#top"
